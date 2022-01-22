a441bfcefa9ddc8a4e27fa77446178738aaf1e1b
IlyaMeli <ilyameli93@gmail.com>
2022-01-23 00:06:06 +0200

    check heroku

--- db/mongoose.js
+++ db/mongoose.js
@@ -5,6 +5,9 @@ dotenv.config();
 
 const PASSWORD = process.env.PASSWORD;
 
+// mongoose.connect(
+//   `mongodb+srv://bankuser2022:${PASSWORD}@bank-cluster.o6tae.mongodb.net/bank-api?retryWrites=true&w=majority`
+// );
 mongoose.connect(
-  `mongodb+srv://bankuser2022:${PASSWORD}@bank-cluster.o6tae.mongodb.net/bank-api?retryWrites=true&w=majority`
+  `mongodb+srv://bankuser2022:MF6yFD3Pety8wgH@bank-cluster.o6tae.mongodb.net/bank-api?retryWrites=true&w=majority`
 );
